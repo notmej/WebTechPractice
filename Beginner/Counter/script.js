56f6a02313e28a2b8944352d64e5e8f9d1144c9a
notmej <yunonaagzamova@gmail.com>
2026-05-14 01:02:31 +0300

    counter done :)

---
+++ Beginner/Counter/script.js
@@ -0,0 +1,26 @@
+const counterText = document.getElementById("toChange");
+const add = document.getElementById("add");
+const reset = document.getElementById("reset");
+var counter = 0;
+
+// No parentheses on onClock call.
+// onClick --> reference
+// onClick() --> execution
+add.addEventListener("click", onClick);
+reset.addEventListener("click", resetb)
+
+//functions
+function onClick() {
+    counter+= 1;
+    display(counter);
+}
+
+function resetb(){
+    counter = 0;
+    display(counter);
+}
+
+// standardized display.
+function display(counter){
+    counterText.textContent = counter;
+}
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This is a repository for my practice of pure HTML, CSS, JS, and whatever else is
 
 This Repo will include completed practice questions with (maybe) notes present. It is for my own use but if you iwsh to have a look or use my questions, you are free to do so :)
 
-enjoy. 
+enjoy.
